49ddd8e738451769f79d95d8ea2873aacf086cc4
jilanglois <56458805+jilanglois@users.noreply.github.com>
2022-02-24 01:02:21 -0800

    debug change

--- util.py
+++ util.py
@@ -340,6 +340,7 @@ def postprocess_qa_predictions(examples, features, predictions,
         for key in examples:
             if key == 'labels':
                 continue
+            print(key)
             example[key] = examples[key][example_index]
         # Those are the indices of the features associated to the current example.
         feature_indices = features_per_example[example_index]
