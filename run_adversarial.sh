8ce6495828d15bd00c1dcf1dac8406356cae7b92
Juan Langlois <jlangloi@stanford.edu>
2022-03-07 15:17:37 -0800

    degub sh file

--- run_adversarial.sh
+++ run_adversarial.sh
@@ -3,4 +3,7 @@
 if [ "$1" = "train_indomain" ]; then
 	python3 adversarial_trainer.py --do-train --dis-lambda 1e-2 --run-name adversarial_0303 --save-dir save --recompute-features
 elif [ "$1" = "train_oodomain" ]; then
-  python3 adversarial_trainer.py --do-train --dis-lambda 1e-2 --run-name adversarial_0303_finetune --load-dir save/adversarial_0303 --save-dir save --recompute-features --train-dir datasets/oodomain_train --train-datasets duorc,race,relation_extraction --val-dir datasets/oodomain_val
+  python3 adversarial_trainer.py --do-train --dis-lambda 1e-2 --run-name adversarial_0303_finetune --load-dir save/adversarial_0303-01 --save-dir save --recompute-features --train-dir datasets/oodomain_train --train-datasets duorc,race,relation_extraction --val-dir datasets/oodomain_val
+else
+	echo "Invalid Option Selected"
+fi
